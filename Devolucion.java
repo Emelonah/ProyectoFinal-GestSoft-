04de0f29b1b409eb16d616261294c7720c8ca67a
JJ-Jan <janmarcos54@gmail.com>
2025-06-23 11:44:07 -0400

    feat: create the Devolucion class

---
+++ Devolucion.java
@@ -0,0 +1,51 @@
+package proyectofinal;
+
+public class Devolucion {
+    private Venta v;
+    private String idDevolucion;
+    private int cantidadDevuelta;
+    private String fechaDevolucion;
+
+    public Devolucion(Venta v, String idDevolucion, int cantidadDevuelta, String fechaDevolucion) {
+        this.v = v;
+        this.idDevolucion = idDevolucion;
+        if (cantidadDevuelta <= v.getCantVenta())
+            this.cantidadDevuelta = cantidadDevuelta;
+        v.getA().setIsDevuelto(true);
+        this.fechaDevolucion = fechaDevolucion;
+    }
+
+    public Venta getV() {
+        return v;
+    }
+
+    public void setV(Venta v) {
+        this.v = v;
+    }
+
+    public String getIdDevolucion() {
+        return idDevolucion;
+    }
+
+    public void setIdDevolucion(String idDevolucion) {
+        this.idDevolucion = idDevolucion;
+    }
+
+    public int getCantidadDevuelta() {
+        return cantidadDevuelta;
+    }
+
+    public void setCantidadDevuelta(int cantidadDevuelta) {
+        this.cantidadDevuelta = cantidadDevuelta;
+    }
+
+    public String getFechaDevolucion() {
+        return fechaDevolucion;
+    }
+
+    public void setFechaDevolucion(String fechaDevolucion) {
+        this.fechaDevolucion = fechaDevolucion;
+    }
+    
+    
+}
