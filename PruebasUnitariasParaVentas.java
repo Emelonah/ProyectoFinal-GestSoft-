5017b3df2e7297dc68d5b4841d247cb8f425fcfb
Erika-RC <erikarodriguezcarrazana@gmail.com>
2025-06-26 22:21:50 +0200

    fix: edit code
    Eliminar código erróneo dentro de la clase.

--- PruebasUnitariasParaVentas.java
+++ PruebasUnitariasParaVentas.java
@@ -10,13 +10,11 @@ import proyectofinal.Venta;
 
 public class PruebasUnitariasParaVentas {
     private Tienda tienda;
-    private ArrayList<Venta> ventas;
-    private ArrayList<Devolucion> devoluciones;
-    private ArrayList<Articulo> inventario;
+    
     
      @Before
   public void setUp(){
-      tienda = new Tienda("Estrellita",inventario, ventas, devoluciones);
+      tienda = new Tienda("Estrellita");
   }
     
     @Test
@@ -25,15 +23,14 @@ public class PruebasUnitariasParaVentas {
      Articulo articulo = new Articulo("blusa","Prendas Superiores","004",12.5);//Se crea un nuevo objeto de tipo Articulo
      //Se le pasan los parametros de la venta
      String idVenta = "001";
-     int cantidadVendida = 2;
-     double totalVenta = 25;
+     int cantidadVenta = 2;
      String fechaVenta = "28/3/25";   
-     Venta venta = new Venta(articulo,idVenta,cantidadVendida,totalVenta,fechaVenta);//Se crea un nuevo objeto de tipo Venta
+     Venta venta = new Venta(articulo,idVenta,cantidadVenta,fechaVenta);//Se crea un nuevo objeto de tipo Venta
      
      
      assertEquals(idVenta,venta.getIdVenta()); //Se genera correctamente el id de la venta
-     assertEquals("004", venta.getArticulo().getIdArticulo()); //Se obtiene el id del articulo
-     assertEquals(cantidadVendida,venta.getCantidadVendida()); //Se almacena la cantidad vendida
+     assertEquals("004", venta.getA().getIdArticulo()); //Se obtiene el id del articulo
+     assertEquals(cantidadVenta,venta.getCantVenta()); //Se almacena la cantidad vendida
      assertEquals(fechaVenta, venta.getFechaVenta()); //Se almacena la fecha de la venta
      assertEquals(12.5 *2, venta.getTotalVenta(),0.0);// Se calcula el total de manera precisa
     
@@ -45,17 +42,16 @@ public class PruebasUnitariasParaVentas {
      Articulo articulo = new Articulo("Camisa","Prendas Superiores","008",10.99);//Se crea un nuevo objeto de tipo Articulo
      //Se le pasan los parametros de la venta
      String idVenta = "009";
-     int cantidadVendida = 5;
-     double totalVenta = 54.95;
+     int cantidadVenta = 5;
      String fechaVenta = "20/2/25";  
         
      //Se crea y agrega un nuevo objeto de tipo Venta
-     Venta venta = new Venta(articulo,idVenta,cantidadVendida,totalVenta,fechaVenta);//Se crea un nuevo objeto de tipo Venta
+     Venta venta = new Venta(articulo,idVenta,cantidadVenta,fechaVenta);//Se crea un nuevo objeto de tipo Venta
      
-     tienda.añadirVenta(venta);
+     tienda.addV(venta);
         
      assertEquals(1,tienda.getVentas().size());//La lista de ventas tiene una venta
-     assertEquals("008",tienda.getVentas().get(0).getArticulo().getIdArticulo());//Se obtiene el id del articulo
+     assertEquals("008",tienda.getVentas().get(0).getA().getIdArticulo());//Se obtiene el id del articulo
      assertFalse(tienda.getVentas().isEmpty());//La lista de ventas no esta vacia
      assertTrue(tienda.getVentas().contains(venta));//La lista de ventas contiene el objeto creado
     } 
@@ -68,11 +64,10 @@ public class PruebasUnitariasParaVentas {
      Articulo articulo = new Articulo("Pantalon","Prendas Inferiores","007",35.69);//Se crea un nuevo objeto de tipo Articulo
      //Se le pasan los parametros de la venta
      String idVenta = "003";
-     int cantidadVendida = 4;
-     double totalVenta = 0;
+     int cantidadVenta = 4;
      String fechaVenta = "15/1/25"; 
      //Se crea un nuevo objeto de tipo Venta y el valor total esperado
-     Venta venta = new Venta (articulo,idVenta,cantidadVendida,totalVenta,fechaVenta);
+     Venta venta = new Venta (articulo,idVenta,cantidadVenta,fechaVenta);
      double totalEsperado = 142.76;
      
      assertEquals(totalEsperado,venta.getTotalVenta(),0.00);//Se valida que el total de la venta coincida con el valor total esperado
@@ -86,8 +81,8 @@ public class PruebasUnitariasParaVentas {
      Articulo articulo = new Articulo("Gorra","Prendas Extra","007",4.5);//Se crea un nuevo objeto de tipo Articulo
      
     //Se crean y agregan dos nuevos objetos de tipo Venta
-     tienda.añadirVenta(new Venta(articulo,"078",3,13.5,"6/8/24"));
-     tienda.añadirVenta(new Venta(articulo,"078",9,18,"6/2/25"));
+     tienda.addV(new Venta(articulo,"078",3,"6/8/24"));
+     tienda.addV(new Venta(articulo,"078",9,"6/2/25"));
      
      assertEquals(2,tienda.getVentas().size());//La lista de ventas tiene dos ventas
      
@@ -98,7 +93,7 @@ public class PruebasUnitariasParaVentas {
     public void registrarVentaConCantidadCero(){
     //Debe lanzar la excepcion IllegalArgumentException   
      Articulo articulo = new Articulo("Falda","Prendas Inferiores","012",16.5);
-     tienda.añadirVenta(new Venta (articulo,"056",0,0,"24/5/25"));//Falla al construir la venta
+     tienda.addV(new Venta (articulo,"056",0,"24/5/25"));//Falla al construir la venta
         
     }
     
@@ -106,20 +101,10 @@ public class PruebasUnitariasParaVentas {
     public void registrarVentaConCantidadNegativa(){
     //Debe lanzar la excepcion IllegalArgumentException   
      Articulo articulo = new Articulo("Licra","Prendas Inferiores","034",8.94);
-     tienda.añadirVenta(new Venta (articulo,"089",-3,26.82,"24/5/25"));//Falla al construir la venta porque la cantidad es negativa
+     tienda.addV(new Venta (articulo,"089",-3,"24/5/25"));//Falla al construir la venta porque la cantidad es negativa
         
     }
-    
-    
-    
-    @Test(expected = IllegalArgumentException.class)
-    public void registrarVentaConTotalVentaNegativo(){
-    //Debe lanzar la excepcion IllegalArgumentException   
-     
-     Articulo articulo = new Articulo("Licra","Prendas Inferiores","034",-8.94);//Se construye el objeto Articulo con precio negativo
-     tienda.añadirVenta(new Venta (articulo,"089",3,-26.82,"24/5/25"));//Falla al construir la venta porque el total de la venta es negativo
-        
-    }
+   
     
     
     
