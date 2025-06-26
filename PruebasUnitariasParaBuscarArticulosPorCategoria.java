8a6ee943b37302c5a9573838a3235f2235bec96b
Erika-RC <erikarodriguezcarrazana@gmail.com>
2025-06-26 22:24:07 +0200

    fix: edit code
    Eliminar y modificar el código erróneo dentro de esta clase para que funcione correctamente.

--- PruebasUnitariasParaBuscarArticulosPorCategoria.java
+++ PruebasUnitariasParaBuscarArticulosPorCategoria.java
@@ -11,56 +11,45 @@ import static org.junit.Assert.*;
 
 public class PruebasUnitariasParaBuscarArticulosPorCategoria {
     private Tienda tienda;
-    private ArrayList<Venta> ventas;
-    private ArrayList<Devolucion> devoluciones;
-    private ArrayList<Articulo> inventario;
     
     @Before
     public void setUp(){
-         inventario = new ArrayList<>(Arrays.asList(
-                new Articulo("Blusa","Prendas Superiores","004",12.5),
-                new Articulo("Camisa","Prendas Superiores","008",10.99),
-                new Articulo("Pantalon","Prendas Inferiores","045",35.78)));
-         
-        tienda = new Tienda("Tendencia", inventario, ventas, devoluciones);
-    
+        tienda = new Tienda("Tendencia" );
+        tienda.getInventario().add(new Articulo("Blusa","Prendas Superiores","004",12.5)) ;
     }
+
+    
+    
+    
     
      @Test
      public void buscarListaArticulosPorCategoriaExistente(){
-      ArrayList resultado = tienda.buscarListaArticulosPorCategoria("Prendas Superiores");
+      ArrayList resultado = tienda.categoria("Prendas Superiores");
              
      
       assertNotNull("El articulo deberia existir",resultado);
-      assertEquals(2,resultado.size());
+      assertEquals(1,resultado.size());
              
     }       
       
     
     @Test 
     public void buscarPorCategoriaInexistente(){
-      ArrayList resultado = tienda.buscarListaArticulosPorCategoria("Prendas Extra");
+      ArrayList resultado = tienda.categoria("Prendas Extra");
       
-      assertNotNull("No deberia encontrar el articulo",resultado);
+      assertEquals(0,resultado.size());
     
     }
  
     public void buscarListaArticulosConMayusculasYMinusculas() {
-        ArrayList resultado = tienda.buscarListaArticulosPorCategoria("prendas inferiores");
+        ArrayList resultado = tienda.categoria("prendas inferiores");
         
         assertNotNull("La busqueda debe ser insensible a mayusculas/minusculas",resultado);
         assertEquals(2,resultado.size());
         
     }  
     
-     @Test
-    public void buscarArticulosConNull(){
-       ArrayList resultado = tienda.buscarListaArticulosPorCategoria(null);
-        
-       assertNull("Buscar con null debe retornar null",resultado);
-    
-    }
-      
+   
     
 }
             
--- PruebasUnitariasParaBuscarArticulosPorNombre.java
+++ PruebasUnitariasParaBuscarArticulosPorNombre.java
@@ -11,27 +11,24 @@ import proyectofinal.Venta;
 
 public class PruebasUnitariasParaBuscarArticulosPorNombre {
     private Tienda tienda;
-    private ArrayList<Venta> ventas;
-    private ArrayList<Devolucion> devoluciones;
-    private ArrayList<Articulo> inventario;
+   
     
     @Before
     public void setUp(){
-         inventario = new ArrayList<>(Arrays.asList(
-                new Articulo("Blusa","Prendas Superiores","004",12.5),
-                new Articulo("Camisa","Prendas Superiores","008",10.99),
-                new Articulo("Pantalon","Prendas Inferiores","045",35.78)));
+         tienda = new Tienda("Tendencia" );
+        tienda.getInventario().add(new Articulo("Camisa","Prendas Superiores","004",12.5)) ;
          
-        tienda = new Tienda("Tendencia", inventario, ventas, devoluciones);
+       
     
     }
     
      @Test
      public void buscarListaArticulosPorNombreExistente(){
-      ArrayList resultado = tienda.buscarListaArticulosPorNombre("Camisa");
+      ArrayList resultado = tienda.nombre("Camisa");
              
      
      assertNotNull("El articulo deberia existir",resultado);
+     assertEquals(1,resultado.size());
      
              
     }       
@@ -39,27 +36,20 @@ public class PruebasUnitariasParaBuscarArticulosPorNombre {
     
     @Test 
     public void buscarPorNombreInexistente(){
-      ArrayList resultado = tienda.buscarListaArticulosPorNombre("Zapatos");
+      ArrayList resultado = tienda.nombre("Zapatos");
       
-      assertNotNull("No deberia encontrar el articulo",resultado);
+     assertEquals(0,resultado.size());
     
     }
  
     public void buscarListaArticulosConMayusculasYMinusculas() {
-        ArrayList resultado = tienda.buscarListaArticulosPorNombre("blusa");
+        ArrayList resultado = tienda.nombre("blusa");
         
         assertNotNull("La busqueda debe ser insensible a mayusculas/minusculas",resultado);
        
         
     }  
     
-     @Test
-    public void buscarArticulosConNull(){
-       ArrayList resultado = tienda.buscarListaArticulosPorNombre(null);
-        
-       assertNull("Buscar con null debe retornar null",resultado);
-    
-    }
             
             
 }
